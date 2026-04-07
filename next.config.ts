eb8a8e433d9633f96d3375021bf98b2b0e73a22e
Yopin <yopinm@gmail.com>
2026-04-07 15:10:33 +0700

    fix(task-1a-022): add allowedDevOrigins for ngrok dev

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-﻿import createNextIntlPlugin from 'next-intl/plugin';
+﻿import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin(
-  './src/i18n/request.ts'
-);
+const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  allowedDevOrigins: ["kira-unpenetrating-uncogently.ngrok-free.dev"],
+};
 
 export default withNextIntl(nextConfig);
